53b493e72653d6d4f17d9a2cc84af4c046488024
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-02-17 21:29:35 -0500

    Basic app setup

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module tech.harmless.fileindexer {
+    // Annotations
+    requires static lombok;
+    requires static org.jetbrains.annotations;
+    requires static com.github.spotbugs.annotations;
+}
---
+++ app/src/main/java/tech/harmless/fileindexer/App.java
@@ -0,0 +1,11 @@
+package tech.harmless.fileindexer;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("NAME: " + BuildConfig.NAME);
+        System.out.println("VERSION: " + BuildConfig.VERSION);
+        System.out.println("DESCRIPTION: " + BuildConfig.DESCRIPTION);
+        System.out.println("COMMIT_HASH: " + BuildConfig.COMMIT_HASH);
+        System.out.println("DEBUG: " + BuildConfig.DEBUG);
+    }
+}
